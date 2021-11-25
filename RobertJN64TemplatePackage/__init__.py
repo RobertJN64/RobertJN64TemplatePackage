7118793b08d44f77100de89782400482e8a99ae7
RobertJN <robertjnies@gmail.com>
2021-11-25 12:24:10 -0500

    document the code

--- RobertJN64TemplatePackage/__init__.py
+++ RobertJN64TemplatePackage/__init__.py
@@ -5,16 +5,34 @@ This code here has some functions that we will call in our tests.
 import os.path as path
 
 
-def add(a, b):
+def add(a: int, b: int):
+    """
+    A simple function for adding 2 numbers
+
+    :param a: first term
+    :param b: second term
+    :return: sum
+    """
     return a + b
 
-def error():
+def error() -> None:
+    """
+    Raises an error for testing purposes
+    """
     raise Exception("HELP THIS IS AN ERROR")
 
 def openFile():
+    """
+    Opens the file 'afile.txt'
+
+    :return: file text
+    """
     with open(path.dirname(__file__) + '/afile.txt') as f:
         return f.read().strip('\n')
 
 def printTable():
+    """
+    Uses the tabulate module to print a table
+    """
     import tabulate as t
     print(t.tabulate([[1,2,3]], ['a', 'b', 'c']))
